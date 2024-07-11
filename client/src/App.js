7a3cb84408f66713e39c4270d24ecbfca912171f
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-11 19:43:48 +0530

    add router and create login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-  return <div className="App">hello world!</div>;
+  return <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
